06fa7a8284dc42be5d1c86b83808fec3c4709570
Koynovigor <igorkoynov2001@gmail.com>
2022-11-23 22:56:21 +0700

    added classes

---
+++ storage/Source.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Storage
+{
+	string address;
+};
+
+class Area
+{
+
+};
+
+class Product
+{
+	string name;
+	string type;
+public:
+	Product(string n, string t, int c) { };
+};
+
+class Report
+{
+
+};
+
+class Date
+{
+	int m_day;
+	int m_month;
+	int m_year;
+
+};
+
+int main()
+{
+
+}
